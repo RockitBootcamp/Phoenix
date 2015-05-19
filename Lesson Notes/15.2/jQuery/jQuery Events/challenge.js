a131c47f950ce74199fec26d8a308d7240173061
Brig Lamoreaux <brig.lamoreaux@apollo.edu>
2015-05-19 12:19:28 -0700

    Add keyDown challenge

--- Lesson Notes/15.2/jQuery/jQuery Events/challenge.js
+++ Lesson Notes/15.2/jQuery/jQuery Events/challenge.js
@@ -1,1 +1,10 @@
-// Challenge 1. Console.log hello world when you click the button
+
+$(function() {
+
+    // Challenge 1. Console.log hello world when you click the button
+
+    // Challenge 2. Console.log "WOW" when you mouseover the anchor
+    
+    // Challenge 3. Console.log "Great" when you keyDown in the input box
+});
+
--- Lesson Notes/15.2/jQuery/jQuery Events/challengeAnswers.js
+++ Lesson Notes/15.2/jQuery/jQuery Events/challengeAnswers.js
@@ -7,3 +7,18 @@
 // $('button').on('click', function(){});
 
 // Step 3. Do you action inside the function
+// $('button').on('click', function(){
+//    console.log("hello world");
+//});
+
+// Challenge 2. Console.log "WOW" when you mouseover the anchor
+// Step 1. Find the element you want to hook onto.
+// $('a')
+
+// Step 2. Write the code to hook a specific event
+// $('a').on('mouseover', function(){});
+
+// Step 3. Do you action inside the function
+// $('a').on('mouseover', function(){
+//    console.log("WOW");
+//});
