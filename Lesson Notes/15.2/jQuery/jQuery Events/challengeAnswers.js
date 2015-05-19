eea1d3f34ea9419acf0cd4dda868827f75c47026
Brig Lamoreaux <brig.lamoreaux@apollo.edu>
2015-05-19 12:07:35 -0700

    Add answers to challenge

---
+++ Lesson Notes/15.2/jQuery/jQuery Events/challengeAnswers.js
@@ -0,0 +1,9 @@
+// Challenge 1. Console.log hello world when you click the button
+
+// Step 1. Find the element you want to hook onto.
+// $('button')
+
+// Step 2. Write the code to hook a specific event
+// $('button').on('click', function(){});
+
+// Step 3. Do you action inside the function
