b2b132e568d5cac781036131b37daf73dc77e557
Brig Lamoreaux <brig.lamoreaux@apollo.edu>
2015-05-19 12:38:47 -0700

    Add challenge for more events

--- Lesson Notes/15.2/jQuery/jQuery Events/challenge.js
+++ Lesson Notes/15.2/jQuery/jQuery Events/challenge.js
@@ -6,5 +6,13 @@ $(function() {
     // Challenge 2. Console.log "WOW" when you mouseover the anchor
     
     // Challenge 3. Console.log "Great" when you keyDown in the input box
+
+    // Challenge 4. Console.log "Boo" when you keyUp in the input box
+
+    // Challenge 5. Console.log "Sup" when the firstname has focus
+
+    // Challenge 6. Console.log $(this) when anything is clicked
+    
 });
+
 
--- Lesson Notes/15.2/jQuery/jQuery Events/challengeAnswers.js
+++ Lesson Notes/15.2/jQuery/jQuery Events/challengeAnswers.js
@@ -12,13 +12,27 @@
 //});
 
 // Challenge 2. Console.log "WOW" when you mouseover the anchor
-// Step 1. Find the element you want to hook onto.
-// $('a')
-
-// Step 2. Write the code to hook a specific event
-// $('a').on('mouseover', function(){});
-
-// Step 3. Do you action inside the function
 // $('a').on('mouseover', function(){
 //    console.log("WOW");
 //});
+
+// Challenge 3 Console.log "Great" when you keyDown in the input box
+// $('input').on('keydown', function(){
+//    console.log("WOW");
+// });
+
+// Challenge 4. Console.log "Boo" when you keyUp in the input box
+// $('input').on('keyup', function(){
+//    console.log("Boo");
+// });
+
+// Challenge 5. Console.log "Sup" when the firstname has focus
+// $('.fn').on('focus', function(){
+//    console.log("Sup");
+// });
+
+// Challenge 6. Console.log $(this) when anything is clicked
+// $('*').on('click', function(){
+//     event.stopPropagation()
+//    console.log($(this));
+// });
