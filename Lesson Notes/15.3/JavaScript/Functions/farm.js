96bfa9b814cff851e083b54fe5ff39f64ff50088
Kevin Lanni <therealklanni@gmail.com>
2015-06-26 18:15:35 -0700

    lessons

--- Lesson Notes/15.3/JavaScript/Functions/farm.js
+++ Lesson Notes/15.3/JavaScript/Functions/farm.js
@@ -1,35 +1,48 @@
-var milkCow = function (name) {
-  console.log('milk ' + name + ', bro')
+// var milkCow = function (name) {
+//   console.log('milk ' + name + ', bro')
+// }
+
+// var getBucket = function () {
+//   console.log('get a bucket')
+// }
+
+// var getDressed = function () {
+//   console.log('put on some clothes, weirdo')
+// }
+
+// var goToChurch = function () {
+//   getDressed()
+//   console.log('go to church')
+// }
+
+// // goToChurch()
+
+// var cows = ['Betsy', 'Bell', 'Spot', 'Alice']
+
+// function startMilking() {
+//   getDressed()
+//   getBucket()
+//   cows.forEach(milkCow)
+// }
+
+// var today = 'Sunday'
+
+// if (today == 'Sunday') {
+//   goToChurch();
+// } else {
+//   startMilking();
+// }
+
+
+
+// depends on getDressed
+function milkCow() {
+  console.log('Milk the cow');
 }
 
-var getBucket = function () {
-  console.log('get a bucket')
+function getDressed(callback) {
+  console.log('Get dressed');
+  callback();
 }
 
-var getDressed = function () {
-  console.log('put on some clothes, weirdo')
-}
-
-var goToChurch = function () {
-  getDressed()
-  console.log('go to church')
-}
-
-// goToChurch()
-
-var cows = ['Betsy', 'Bell', 'Spot', 'Alice']
-
-function startMilking() {
-  getDressed()
-  getBucket()
-  cows.forEach(milkCow)
-}
-
-var today = 'Sunday'
-
-if (today == 'Sunday') {
-  goToChurch();
-} else {
-  startMilking();
-}
-
+getDressed(milkCow);
