55e1d08130788d589d02b8b1d80cb42450411ebc
Kevin Lanni <therealklanni@gmail.com>
2015-09-17 11:54:13 -0700

    PHP strings test

---
+++ Acid Tests/PHP/Strings/index.php
@@ -0,0 +1,29 @@
+<?php
+
+// Fill in the variables below with appropriate words
+$name = "";
+$adverb = "";
+$noun = "";
+$exclamation = "";
+$adjective1 = "";
+$adjective2 = "";
+$noun1 = "";
+$noun2 = "";
+
+// Then fix each string to see the funny phrases!
+
+$string = '$exclamation! he said $adverb as he jumped into his convertible $noun and drove off with his $adjective1 wife.';
+
+echo $string;
+
+$string = "please excuse" + $name + "who is far too" $adjective2 + to attend + $noun2 + 'class.'
+
+echo $string;
+
+$string = $noun1 "is the new" $noun2;
+
+echo $string;
+
+$string = '$adjective3' . ' had a '. '$noun3' . ', E-I-E-I-O';
+
+echo $string;
