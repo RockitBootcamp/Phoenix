f30fce4289b50695c5b8e285657d59c5d708a291
Kevin Lanni <therealklanni@gmail.com>
2015-09-25 16:14:52 -0700

    semicolons

--- Lesson Notes/15.6/JavaScript/M2 Review/index.js
+++ Lesson Notes/15.6/JavaScript/M2 Review/index.js
@@ -19,35 +19,35 @@ asdfasfasdf`;
 
 // console.log(foo);
 
-var sum = 5 + '8'
+var sum = 5 + '8';
 // console.dir(+'8')
 // console.dir(Number('8'))
 // console.dir(~~'8.2')
 // console.dir(-'8.2')
 // console.dir(parseInt('09'))
-console.dir(parseInt('101', 2))
-console.dir(parseInt('9A', 16))
+console.dir(parseInt('101', 2));
+console.dir(parseInt('9A', 16));
 
-console.dir(parseInt('8.9', 10))
+console.dir(parseInt('8.9', 10));
 
-console.dir(parseFloat('8.9', 10))
-console.dir(sum)
+console.dir(parseFloat('8.9', 10));
+console.dir(sum);
 
-console.dir((.2 + .1).toFixed(1)) // WAT
-console.dir(9999999999999999)
+console.dir((.2 + .1).toFixed(1)); // WAT
+console.dir(9999999999999999);
 
-console.dir((2).toFixed(2))
-console.dir(Number(2).toFixed(4))
+console.dir((2).toFixed(2));
+console.dir(Number(2).toFixed(4));
 
-console.dir('hello world'.substr(6, 1))
-console.dir('hello world'.slice(6, 7))
-console.dir('hello world'.charAt(6))
-console.dir(String.fromCharCode(34))
+console.dir('hello world'.substr(6, 1));
+console.dir('hello world'.slice(6, 7));
+console.dir('hello world'.charAt(6));
+console.dir(String.fromCharCode(34));
 
-console.log(1 - Infinity)
+console.log(1 - Infinity);
 
-console.dir('' + 5)
-console.dir(Number('5'))
-console.dir(String(5))
-console.dir(Boolean(-1))
+console.dir('' + 5);
+console.dir(Number('5'));
+console.dir(String(5));
+console.dir(Boolean(-1));
 
