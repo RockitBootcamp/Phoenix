5c0562f788e1db366eb250f2ac7a66693e7f793a
Brad Westfall <brad@azpixels.com>
2015-11-04 16:09:57 -0700

    full CRUD

--- Lesson Notes/15.6/life/main.js
+++ Lesson Notes/15.6/life/main.js
@@ -7,11 +7,29 @@ $(function() {
             city: $('.city').val()
         }
         
-        $.post('http://127.0.0.1:3000/teams', payload).then(function() {
-            loadTeams();
-        }).fail(function() {
-            // handle things not going well
-        })
+        if ($('.teamId').val() === '') {
+
+            $.post('http://127.0.0.1:3000/teams', payload).then(function() {
+                loadTeams();
+            }).fail(function() {
+                // handle things not going well
+            })
+
+        } else {
+
+            $.ajax({
+                url: 'http://localhost:3000/teams/' + $('.teamId').val(),
+                data: payload,
+                type: 'PUT'
+            }).then(function() {
+                loadTeams();
+            })
+
+        }
+
+        $('.name').val('');
+        $('.city').val('');
+        $('.teamId').val('');
 
         return false;
     });
@@ -34,5 +52,36 @@ $(function() {
 
     // Initial Call
     loadTeams();
+
+
+    $('ul').on('click', 'button.delete', function() {
+
+        var id = $(this).parents('li').data('id')
+
+        $.ajax({
+            url: 'http://localhost:3000/teams/' + id,
+            type: 'DELETE'
+        }).then(function() {
+            loadTeams();
+        })
+
+    });
+
+
+
+    $('ul').on('click', 'button.update', function() {
+
+        var id = $(this).parents('li').data('id')
+
+        $.get('http://localhost:3000/teams/' + id).then(function(team) {
+
+            $('.name').val(team.name)
+            $('.city').val(team.city)
+            $('.teamId').val(team.id)
+
+        })
+
+
+    });
 
 })
